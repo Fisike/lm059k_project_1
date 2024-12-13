605a421b500b455ef4805d30e2faf459d7b0a7aa
Fisike <48595812+Fisike@users.noreply.github.com>
2024-12-13 01:56:22 +0100

    Create test_unit.py

---
+++ test_unit.py
@@ -0,0 +1,16 @@
+# test_unit.py
+
+import unittest
+from program import Calculator
+
+class TestCalculatorUnit(unittest.TestCase):
+    def test_add(self):
+        calc = Calculator()
+        self.assertEqual(calc.add(2, 3), 5)
+
+    def test_multiply(self):
+        calc = Calculator()
+        self.assertEqual(calc.multiply(2, 3), 6)
+
+if __name__ == '__main__':
+    unittest.main()
